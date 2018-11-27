1c2e7acbe7f3c9df8b4817c9e12d55bdfd721896
Lucy <42339395+LucyChen0228@users.noreply.github.com>
2018-11-27 22:10:53 +0800

    Add files via upload

---
+++ Face.java
@@ -0,0 +1,17 @@
+package lab5;
+
+public class Face {
+
+	
+	   public static void main(String args[]){
+	     Add3 a=new Add3();
+	     for(int i=0;i<5;i++)
+	       System.out.println(a.getnext());
+	     System.out.println();
+	a.setstart(10);
+	         for(int i=0;i<5;i++)
+	           System.out.println(a.getnext());
+	         }
+	     }
+
+
